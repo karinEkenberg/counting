e9d0d7e2e4e516d22d747251380c1b816aecc2ca
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2023-11-29 17:29:55 +0100

    Add files via upload

---
+++ lalalu2/lalalu2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lalalu2
+{
+    class Program
+    {
+        static int RäknaBokstäver(string taEmot)
+        {
+           // int räknare = taEmot.Length;
+            return taEmot.Length;
+        }
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.WriteLine("Var god skriv in ett värde (Lämna blankt för att avsluta).");
+                string input = Console.ReadLine();  
+                if (input == "")
+                {
+                    isRunning = false;
+                }
+                else
+                {
+                    Console.WriteLine("Du skrev värdet: " + input);
+                    Console.WriteLine("Det har antalet bokstäver: " + RäknaBokstäver(input));
+                }
+            }
+
+
+
+        }
+    }
+}
